d30dbca4e303ce7a767695ccbb705479a732f926
Luis E. Lopez <ux@lelopez.io>
2018-08-21 12:57:00 -0500

    first problem

---
+++ hw0-cosc-4377/CombineFiles.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CombineFiles {
+  public static void main(String[] args) throws FileNotFoundException {
+    
+    // Temporarly stores an int on a line
+    int lineInt;
+
+    // program must be given at least one input file and one output file
+    if(args.length > 1) {
+      int fileAmnt = args.length - 1;
+      int fileSums[] = new int[fileAmnt];
+      int fileIndex[] = new int[fileAmnt];
+      Scanner[] file = new Scanner[fileAmnt];
+      ArrayList<ArrayList<Integer>> fileList = new ArrayList<ArrayList<Integer>>();
+      ArrayList<Integer> intList = new ArrayList<Integer>();
+
+      // should check if output file already exist, ask if you wanna overwrite
+      
+      // iterate through files and find their sums
+      for (int i = 0; i < fileAmnt; i++) {
+	      try {
+          // will have to make an array of scanners with the file contents
+          // will end up swapping these around instead of the index array since
+          // I wont need to re-read the file
+		      file[i] = new Scanner(new File(args[i]));
+
+          fileSums[i] = 0;
+		      while(file[i].hasNextInt()){
+			      lineInt = file[i].nextInt();
+            fileSums[i] += lineInt;
+
+            intList.add(lineInt);
+            // fileList.get(i).add(lineInt);
+		      }
+	      }
+        // flag files not found
+	      catch (FileNotFoundException ex) {
+		      System.out.println("File does not exist within directory");
+          fileSums[i] = -1;
+	      }
+        
+        System.out.printf("Sum: %d\n", fileSums[i]);
+        fileIndex[i] = i;
+        fileList.add(intList);
+        intList = new ArrayList<Integer>();
+      }
+
+      
+      // iterate through file sums' and order their indecies
+      for (int i = 0; i < fileAmnt; i++) {
+        for (int j = i + 1; j < fileAmnt; j++) {
+          int temp = 0;
+          Scanner tempFile;
+          
+          if (fileSums[i] > fileSums[j]) {
+            temp = fileSums[i];
+            fileSums[i] = fileSums[j];
+            fileSums[j] = temp;
+
+            temp = fileIndex[i];
+            fileIndex[i] = fileIndex[j];
+            fileIndex[j] = temp;  
+          }
+        }
+      } 
+
+      for (int i = 0; i < fileAmnt; i++) {
+        System.out.printf("Order: %d\n", fileIndex[i]);
+      }
+
+      for (int i = 0; i < fileList.size(); i++) {
+        System.out.printf("From index %d: \n", fileIndex[i]);
+        Iterator<Integer> integerIterator = fileList.get(fileIndex[i]).iterator();
+        while(integerIterator.hasNext()) {
+          System.out.printf(": %d\n", integerIterator.next());
+        }
+        System.out.printf("\n\n");
+      }
+    }
+  }
+}
